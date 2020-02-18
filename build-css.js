71ab95686988b62a3d3f9f56f362abe322b87772
tegner <jtegner@gmail.com>
2020-02-18 10:25:41 +0100

    new browserslist

--- build-css.js
+++ build-css.js
@@ -1,44 +1,45 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const pkg = require("./package.json");
+const pkg = require('./package.json');
 
-const postcss = require("postcss");
-const discardDuplicates = require("postcss-discard-duplicates")();
-const discardUnused = require("postcss-discard-unused")();
-const mergeRules = require("postcss-merge-rules")();
-const cssnano = require("cssnano")();
-const postcssCustomMedia = require("postcss-custom-media");
-const presetEnv = require("postcss-preset-env");
+const postcss = require('postcss');
+const discardDuplicates = require('postcss-discard-duplicates')();
+const discardUnused = require('postcss-discard-unused')();
+const mergeRules = require('postcss-merge-rules')();
+const cssnano = require('cssnano')();
+const postcssCustomMedia = require('postcss-custom-media');
+const presetEnv = require('postcss-preset-env');
 const runtimeArguments = process.argv.slice(2);
 
-const TEMPVERSION = "./.tempversion";
+const TEMPVERSION = './.tempversion';
+
+const importFrom = [
+  './node_modules/@ekstra-bladet/eb-colors/dist/eb-colors-vars-rgb.css',
+  './node_modules/@ekstra-bladet/eb-colors/dist/eb-colors-css-vars.css',
+  './node_modules/@ekstra-bladet/eb-fonts/dist/eb-fontvars-desktop.css',
+  './src/_variables.css',
+  './src/_custom-mediaqueries.css'
+];
 
 const cssnextObject = {
-  browsers: "last 2 versions, ios >= 6, ie > 10",
+  browsers: 'ie 11',
   features: {
-    "nesting-rules": true
+    'nesting-rules': true
   },
-  importFrom: [
-    // "./node_modules/@ekstra-bladet/eb-colors/dist/eb-colors-vars-rgb.css",
-    // "./node_modules/@ekstra-bladet/eb-colors/dist/eb-colors-css-vars.css",
-    // "./node_modules/@ekstra-bladet/eb-fonts/dist/eb-fontvars-desktop.css",
-    // "./src/_variables.css",
-    "./src/_custom-mediaqueries.css"
-  ],
+  importFrom,
   preserve: false,
   stage: 0,
   warnForDuplicates: false
 };
 
 const customMediaOptions = {
-  importFrom: ["./src/_custom-mediaqueries.css"],
+  importFrom: ['./src/_custom-mediaqueries.css'],
   preserve: false
 };
 
 const readFolder = (folderName, filesToFind, array, lvl = 0) => {
   fs.readdirSync(folderName, { withFileTypes: true }).forEach(output => {
-    // console.log("output.isFile()", output.isFile(), output.name);
     if (output.isFile() && lvl !== 0) {
       if (path.extname(output.name).toLowerCase() === filesToFind) {
         array.push(`${folderName}/${output.name}`);
@@ -50,25 +51,40 @@ const readFolder = (folderName, filesToFind, array, lvl = 0) => {
   });
 };
 
+const getOptions = args => {
+  const options = {
+    build: 'nonie',
+    watching: false
+  };
+  args.forEach(arg => {
+    if (arg.indexOf('watcher') !== -1) {
+      options.watching = true;
+    }
+    if (arg.indexOf('build=ie') !== -1) {
+      options.build = 'ie';
+    }
+  });
+  return options;
+};
+
 const buildCSS = async args => {
   try {
-    const WATCHING = args[0] ? args[0].indexOf("watcher") !== -1 : "";
     const startTime = new Date().getTime();
-    const srcFolder = "./src";
-    const cssFilesToRead = [
-      "./node_modules/@ekstra-bladet/eb-colors/dist/eb-colors-vars-rgb.css",
-      "./node_modules/@ekstra-bladet/eb-colors/dist/eb-colors-css-vars.css",
-      "./node_modules/@ekstra-bladet/eb-fonts/dist/eb-fontvars-desktop.css",
-      "./src/_variables.css",
-      "./src/_custom-mediaqueries.css"
-    ];
-    const fileTypeToFind = ".css";
+
+    const options = getOptions(args);
+    const srcFolder = './src';
+    const cssFilesToRead = options.build === 'ie' ? [] : importFrom;
+    const postcssPlugins =
+      options.build === 'ie' ? [presetEnv(cssnextObject)] : [postcssCustomMedia(customMediaOptions)];
+    const fileTypeToFind = '.css';
+    console.log(cssnextObject.browsers);
     /**
      * Find all css files in src folder
      */
     readFolder(srcFolder, fileTypeToFind, cssFilesToRead);
-    const outFolder = "dist";
-    const outputFile = `${outFolder}/eb-designsystem--nonie.css`;
+    const outFolder = 'dist';
+    const outputFileName = `eb-designsystem--${options.build}.css`;
+    const outputFile = `${outFolder}/${outputFileName}`;
 
     if (!fs.existsSync(outFolder)) {
       fs.mkdirSync(outFolder);
@@ -79,49 +95,44 @@ const buildCSS = async args => {
     cssFilesToRead.forEach(fileName => {
       readFileContent.push(fs.readFileSync(fileName));
     });
-    const css = readFileContent.join("");
-    let tempversionBuild = "";
-    if (WATCHING) {
+    const css = readFileContent.join('');
+    let tempversionBuild = '';
+    if (options.watching) {
       if (fs.existsSync(TEMPVERSION)) {
-        const readVersion = parseInt(
-          fs.readFileSync(TEMPVERSION).toString(),
-          10
-        );
+        const readVersion = parseInt(fs.readFileSync(TEMPVERSION).toString(), 10);
         tempversionBuild = readVersion + 1;
       } else {
         tempversionBuild = 0;
       }
     }
     const branchInfo = fs
-      .readFileSync(".git/HEAD")
+      .readFileSync('.git/HEAD')
       .toString()
       .trim()
-      .split("/");
+      .split('/');
     const branch = branchInfo[branchInfo.length - 1];
-    let rev = require("child_process")
-      .execSync("git rev-parse HEAD")
+    let rev = require('child_process')
+      .execSync('git rev-parse HEAD')
       .toString()
       .trim();
 
     const date = new Date();
-    const dateString = `${date.toLocaleDateString(
-      "da"
-    )} ${date.toLocaleTimeString("da")}`;
+    const dateString = `${date.toLocaleDateString('da')} ${date.toLocaleTimeString('da')}`;
 
-    let version = `${pkg.name} version ${pkg.version} built on ${dateString} on branch "${branch}" at revision "${rev}"`;
-    if (tempversionBuild !== "") {
+    let version = `${outputFileName} version ${pkg.version} built on ${dateString} on branch "${branch}" at revision "${rev}"`;
+    if (tempversionBuild !== '') {
       version = `${version} | TEMP VERSION: ${tempversionBuild}`;
     }
 
     await postcss([
-      postcssCustomMedia(customMediaOptions),
+      ...postcssPlugins,
       discardDuplicates,
       discardUnused,
       mergeRules
       // cssnano
     ])
       .process(css, {
-        from: "undefined"
+        from: 'undefined'
       })
       .then(result => {
         try {
@@ -130,8 +141,8 @@ const buildCSS = async args => {
             return true;
           });
           console.log(version);
-          if (WATCHING) {
-            console.log(" ... write tempversionBuild", tempversionBuild);
+          if (options.watching) {
+            console.log(' ... write tempversionBuild', tempversionBuild);
             fs.writeFile(TEMPVERSION, tempversionBuild, () => {
               return true;
             });
@@ -143,9 +154,9 @@ const buildCSS = async args => {
       });
 
     const endTime = new Date().getTime();
-    console.log("it took?", (endTime - startTime) / 1000);
+    console.log('it took?', (endTime - startTime) / 1000);
   } catch (err) {
-    console.error("ERROR: build css", err);
+    console.error('ERROR: build css', err);
     process.exit(1);
   }
 };
