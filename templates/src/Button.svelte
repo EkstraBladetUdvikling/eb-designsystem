a5c8cfedea9a179f21e98df4854f9beeb806f0e4
tegner <jesper.h.tegner@jppol.dk>
2021-02-09 19:15:59 +0100

    svelte install

---
+++ templates/src/Button.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let classExtension;
+  export let text;
+  export let type;
+
+  let cssClass = `button ${classExtension}`;
+  switch (type) {
+    case 'solid':
+      cssClass = `${cssClass} button--solid`;
+      break;
+    case 'link':
+      cssClass = `${cssClass} button--link`;
+      break;
+  }
+</script>
+
+<button class={cssClass}>
+  <slot name="iconleft"></slot>
+  {#if text}
+    <span>{text}</span>
+  {/if}
+  <slot name="iconright"></slot>
+</button>
