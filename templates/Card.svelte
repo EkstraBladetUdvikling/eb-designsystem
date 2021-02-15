18f09401b9d499f0c975a4b7ed994a35477aa957
tegner <jesper.h.tegner@jppol.dk>
2021-02-15 13:10:38 +0100

    svelte card

---
+++ templates/Card.svelte
@@ -0,0 +1,15 @@
+<script>
+
+  export let href;
+
+</script>
+
+{#if href}
+  <a href={href} class="card">
+    <slot></slot>
+  </a>
+{:else}
+  <div class="card">
+    <slot></slot>
+  </div>
+{/if}
