5a622e69ba8968ad90c39102a69f6af2c99ee230
tegner <jesper.h.tegner@jppol.dk>
2021-02-22 15:36:11 +0100

    Highlight (#68)
    * correcting build and button documentation
    
    * skal du også have kode?
    
    * buttons up?
    
    * similar
    
    * removed hello
    
    * indention

--- documentation/components/Button.svelte
+++ documentation/components/Button.svelte
@@ -1,47 +1,120 @@
 <script lang="ts">
+  import Code from './Code.svelte';
+
   import Button from '../../src/_components/button';
   import Icon from '../../src/_components/icon';
 </script>
 
-<div class="grid-width--small">
-  <h1 style="color:#12507b;font-weight: bolder">Buttons</h1>
-  <Button>Base button</Button>
-  <Button extension="solid">Button solid</Button>
-  <Button extension="link">Button link</Button>
-  <Button extension={['big', 'solid']}>Button big</Button>
+<h1 style="color:#12507b;font-weight: bolder">Buttons</h1>
 
-  <code>
-    {`<Button>Base button</Button>`}<br />
-    {'<Button extension="solid">Solid button</Button>'}<br />
-    {'<Button extension="link">Button link</Button>'}
-  </code>
+<Code language="typescript">
+  {`import Badge from './src/_components/badge/Badge.svelte';`}
+</Code>
 
-  <h3>Styles</h3>
+<Button>Base button</Button>
+<Code>
+  {`<Button>Base button</Button>`}
+</Code>
 
-  <Button type="primary">Button primary</Button>
-  <Button type="secondary">Button secondary</Button>
-  <Button type="accept">Button accept</Button>
-  <Button type="cancel">Button cancel</Button>
-
-  <code>
-    {'<Button type="primary">Button primary</Button>'}<br />
-    {'<Button type="secondary">Button secondary</Button>'}<br />
-    {'<Button type="accept">Button accept</Button>'}<br />
-    {'<Button type="cancel">Button cancel</Button>'}
-  </code>
-
-  <h3>Buttons with Icon</h3>
-  <Button type="primary">
-    <Icon name="ekstrabladet" style="height:20px;" />
-    <span>button with icon right</span>
+<h3>extension attribute</h3>
+<p>options</p>
+<ul>
+  <li>solid</li>
+  <li>link</li>
+  <li>icon</li>
+  <li>big</li>
+</ul>
+<div class="flex">
+  <Button className="margin-m" extension="solid">Button solid</Button>
+  <Button className="margin-m" extension="link">Button link</Button>
+  <Button className="margin-m" extension="icon">
+    <span style="font-size: 30px">&times;</span>
   </Button>
-
-  <Button type="primary">
-    <span>button with icon left</span>
-    <Icon name="ekstrabladet" style="height:20px;" />
+  <Button className="margin-m" extension={'big'}>Button big</Button>
+</div>
+<Code>
+  {`
+  <Button className="margin-m" extension="solid">Button solid</Button>
+  <Button className="margin-m" extension="link">Button link</Button>
+  <Button className="margin-m" extension="icon">
+    <span style="font-size: 30px">&times;</span>
   </Button>
+  <Button className="margin-m" extension="big">Button big</Button>
+  `}
+</Code>
 
-  <Button type="primary" extension="icon">
+<h3>extension attribute combinations</h3>
+<p><b>big</b> can be combined with the other three extensions</p>
+<div class="flex">
+  <Button className="margin-m" extension={['big', 'solid']}>Button big solid</Button>
+  <Button className="margin-m" extension={['big', 'link']}>Button big link</Button>
+  <Button className="margin-m" extension={['big', 'icon']}>
     <span style="font-size: 30px">&times;</span>
   </Button>
 </div>
+
+<Code>
+  {`
+  <Button className="margin-m" extension="big">Button big</Button>
+  <Button className="margin-m" extension="{['big', 'solid']}">Button big solid</Button>
+  <Button className="margin-m" extension="{['big', 'link']}">Button big link</Button>
+  `}
+</Code>
+
+<h3>Styles</h3>
+<div class="flex">
+  <Button className="margin-m" type="primary">Button primary</Button>
+  <Button className="margin-m" type="secondary">Button secondary</Button>
+  <Button className="margin-m" type="accept">Button accept</Button>
+  <Button className="margin-m" type="cancel">Button cancel</Button>
+</div>
+
+<Code>
+  {`
+  <Button className="margin-m" type="primary">Button primary</Button>
+  <Button className="margin-m" type="secondary">Button secondary</Button>
+  <Button className="margin-m" type="accept">Button accept</Button>
+  <Button className="margin-m" type="cancel">Button cancel</Button>
+  `}
+</Code>
+
+<h3>Buttons with Icon</h3>
+<Button className="margin-m">
+  <Icon name="ekstrabladet" style="height:20px;" />
+  <span>button with icon right</span>
+</Button>
+<Code>
+  {`
+  <Button className="margin-m">
+    <Icon name="ekstrabladet" style="height:20px;" />
+    <span>button with icon right</span>
+  </Button>
+  `}
+</Code>
+
+<Button className="margin-m">
+  <span>button with icon left</span>
+  <Icon name="ekstrabladet" style="height:20px;" />
+</Button>
+<Code>
+  {`
+  <Button className="margin-m">
+    <span>button with icon right</span>
+    <Icon name="ekstrabladet" style="height:20px;" />
+  </Button>
+  `}
+</Code>
+
+<Button className="margin-m" extension="icon">
+  <span style="font-size: 30px">&times;</span>
+</Button>
+<Button className="margin-m" extension={['icon', 'solid']}>
+  <span style="font-size: 30px">&times;</span>
+</Button>
+<Code>
+  {`
+  <Button className="margin-m" extension="icon">
+    <span style="font-size: 30px">&times;</span>
+  </Button>
+  `}
+</Code>
--- documentation/routes/routes.ts
+++ documentation/routes/routes.ts
@@ -1,6 +1,7 @@
 import Home from './Home.svelte';
 import Accordion from '../components/Accordion.svelte';
 import ArticleCard from '../components/ArticleCard.svelte';
+import Badge from '../components/Badge.svelte';
 import Button from '../components/Button.svelte';
 import Card from '../components/Card.svelte';
 import Icon from '../components/Icon.svelte';
@@ -11,6 +12,7 @@ const routes = [
   { link: '/', title: 'Overblik', component: Home },
   { link: '/components/accordion', title: 'Accordion', type: 'component', component: Accordion },
   { link: '/components/articlecard', title: 'Article Card', type: 'component', component: ArticleCard },
+  { link: '/components/badge', title: 'Badge', type: 'component', component: Badge },
   { link: '/components/button', title: 'Button', type: 'component', component: Button },
   { link: '/components/card', title: 'Card', type: 'component', component: Card },
   { link: '/components/icon', title: 'Icon', type: 'component', component: Icon },
