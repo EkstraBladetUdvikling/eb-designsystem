636ed9a48d6ab125a56cc83ad98aa157db56a386
tegner <jesper.h.tegner@jppol.dk>
2021-02-23 11:52:28 +0100

    icons

--- documentation/components/Icon.svelte
+++ documentation/components/Icon.svelte
@@ -5,3 +5,35 @@
 <div class="grid-width--small">
   <h1 style="color:#12507b;font-weight: bolder">Icon library</h1>
 </div>
+
+<Icon className="margin-s" style="width: 20px;" name="angle_down_pro" />
+<Icon className="margin-s" style="width: 20px;" name="angle_left_pro" />
+<Icon className="margin-s" style="width: 20px;" name="angle_up_pro" />
+<Icon className="margin-s" style="width: 20px;" name="article" />
+<Icon className="margin-s" style="width: 20px;" name="check_pro" />
+<Icon className="margin-s" style="width: 20px;" name="credit_card" />
+<Icon className="margin-s" style="width: 20px;" name="ebplus_icon" />
+<Icon className="margin-s" style="width: 20px;" name="ebplus_logo_hvid" />
+<Icon className="margin-s" style="width: 20px;" name="ebplus_logo_sort" />
+<Icon className="margin-s" style="width: 20px;" name="ebplus_sort" />
+<Icon className="margin-s" style="width: 20px;" name="ekstrabladet" />
+<Icon className="margin-s" style="width: 20px;" name="envelope" />
+<Icon className="margin-s" style="width: 20px;" name="gallery" />
+<Icon className="margin-s" style="width: 20px;" name="headphones" />
+<Icon className="margin-s" style="width: 20px;" name="headset" />
+<Icon className="margin-s" style="width: 20px;" name="lock" />
+<Icon className="margin-s" style="width: 20px;" name="medielogin" />
+<Icon className="margin-s" style="width: 20px;" name="menu-bars" />
+<Icon className="margin-s" style="width: 20px;" name="miteb-regular" />
+<Icon className="margin-s" style="width: 20px;" name="miteb-solid" />
+<Icon className="margin-s" style="width: 20px;" name="mobil_fone" />
+<Icon className="margin-s" style="width: 20px;" name="mobilepay_icon" />
+<Icon className="margin-s" style="width: 20px;" name="mobilepay_logo" />
+<Icon className="margin-s" style="width: 20px;" name="newspaper" />
+<Icon className="margin-s" style="width: 20px;" name="play-circle-light" />
+<Icon className="margin-s" style="width: 20px;" name="play-icon" />
+<Icon className="margin-s" style="width: 20px;" name="tag-regular" />
+<Icon className="margin-s" style="width: 20px;" name="tag-solid" />
+<Icon className="margin-s" style="width: 20px;" name="tags-regular" />
+<Icon className="margin-s" style="width: 20px;" name="tags-solid" />
+<Icon className="margin-s" style="width: 20px;" name="vnameeo" />
--- src/_components/icon/Icon.svelte
+++ src/_components/icon/Icon.svelte
@@ -1,18 +1,92 @@
-<script>
-  export let className;
-  export let name;
-  export let style;
-  export let viewBox;
+<script lang="ts">
+  export let className: string;
+  export let name: string;
+  export let style: string;
+  export let viewBox: string;
+  export let type: 'svg' | 'fa' = 'svg';
 
   if (!viewBox) {
     switch (name) {
-      case 'ekstrabladet':
-      viewBox = '0 0 100 88';
+      case 'angle_down_pro':
+        viewBox = '0 0 256 149';
         break;
+      case 'angle_left_pro':
+        viewBox = '0 0 149 256';
+        break;
+      case 'angle_up_pro':
+        viewBox = '0 0 256 149';
+        break;
+      case 'article':
+        viewBox = '0 0 51 47';
+        break;
+      case 'check_pro':
+        viewBox = '0 0 442.6 335.1';
+        break;
+      case 'credit_card':
+        viewBox = '0 0 576 448';
+        break;
+      case 'ebplus_logo_hvid':
+      case 'ebplus_logo_sort':
+        viewBox = '0 0 595.2 356';
+        break;
+      case 'ebplus_sort':
+        viewBox = '0 0 595.3 597.4';
+        break;
+      case 'ekstrabladet':
+        viewBox = '0 0 100 88';
+        break;
+      case 'envelope':
+        viewBox = '0 0 512 384';
+        break;
+      case 'lock':
+        viewBox = '0 0 448 512';
+        break;
+      case 'medielogin':
+        viewBox = '0 0 63.6 81.5';
+        break;
+      case 'mobil_fone':
+        viewBox = '0 0 320 512';
+        break;
+      case 'mobilepay_icon':
+        viewBox = '0 0 20 21';
+        break;
+      case 'mobilepay_logo':
+        viewBox = '0 0 215 69';
+        break;
+      case 'newspaper':
+        viewBox = '0 0 42 39';
+        break;
+      case 'tags-regular':
+        viewBox = '0 0 640 512';
+        break;
+      case 'tags-solid':
+        viewBox = '0 0 640 512';
+        break;
+      case 'video':
+        viewBox = '0 0 51 48';
+        break;
+      case 'ebplus_icon':
+      case 'gallery':
+      case 'headphones':
+      case 'headset':
+      case 'menu-bars':
+      case 'miteb-regular':
+      case 'miteb-solid':
+      case 'play-circle-light':
+      case 'play-icon':
+      case 'tag-regular':
+      case 'tag-solid':
+      default:
+        viewBox = '0 0 50 50';
     }
   }
 </script>
 
-<svg viewBox="{viewBox}" style="{style}" class="{className}">
-  <use href="eb.svg#{name}"></use>
-</svg>
+<div>#{name}</div>
+{#if type === 'svg'}
+  <svg {viewBox} {style} class={className}>
+    <use href="eb.svg#{name}" />
+  </svg>
+{:else}
+  <i class={className} aria-hidden="true" />
+{/if}
