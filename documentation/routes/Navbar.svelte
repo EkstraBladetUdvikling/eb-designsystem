1e0c2f9343d763514da0ef11ca92b3340701ac3f
tegner <jesper.h.tegner@jppol.dk>
2021-02-23 14:40:28 +0100

    navbar

--- documentation/routes/Navbar.svelte
+++ documentation/routes/Navbar.svelte
@@ -1,11 +1,7 @@
-<script lang="ts">
-  import Icon from '../../src/_components/icon';
-</script>
-
 <div class="navmenu-container position-fixed width-1of1 margin-xl--b">
   <nav class="navmenu flex flex-align--center padding-xl--rl">
     <a href="/" class="margin-m--t margin-m--r">
-      <img src="ekstrabladet.svg" style="height:70px;" />
+      <img alt="" src="ekstrabladet.svg" style="height:70px;" />
     </a>
     <span class="flex--grow width-1of1 color--graa1">Ekstra Bladet Design System</span>
 
@@ -25,9 +21,9 @@
     transition: .5s;
     }
     .navmenu-container {
-    top: 0;
+      top: 0;
     left: 0;
-      z-index: 9999;
+    z-index: 9999;
 
     background-color: var(--color--white);
     box-shadow: 0 0 10px 0 rgb(0 0 0 / 20%);
