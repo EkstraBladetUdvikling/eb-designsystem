56228d18797fd129f0fa2f804def1707e8475eb9
MathiasNordby <mathias_nordby@hotmail.com>
2021-03-03 11:10:57 +0100

    Utility doc flow

--- documentation/utilities/Animation/Animation.svelte
+++ documentation/utilities/Animation/Animation.svelte
@@ -19,8 +19,59 @@
 <div class="grid-width--large">
   <h1 class="color--eb">Animation</h1>
   <h3>Anvendelse af animationer</h3>
-  <p>Animationer anvendes ved tilføjelse af class: <code>animation-navnPåAnimation</code></p>
+  <p>Animationer anvendes ved tilføjelse af class: <code>className="animation-navnPåAnimation"</code></p>
   <p>Denne class kan anvendes på tværs af vores komponenter</p>
+  <p>
+    <b>OBS:</b> anvendes en animation direkte på et html-element skal det anvendes på følgende måde:
+    <code>class="animation-navnPåAnimation"</code>
+  </p>
+  <h3>Eksempler på animationer</h3>
+  <PillNavigation>
+    <div class="flex flex-justify--end width-1of1">
+      <PillList>
+        <Pill><i class="fas fa-cubes" /></Pill>
+        <Pill><i class="fas fa-code" /></Pill>
+      </PillList>
+    </div>
+    <PillContent>
+      <div class="flex grid-width--small">
+        <ArticleCard
+          className="animation-fogwave"
+          href={article.href}
+          media={{ src: 'https://via.placeholder.com/610x343&text=610x343' }}
+          section={article.section}
+          timestamp={article.timestamp}
+          title={article.title}
+        />
+      </div>
+      <div class="flex grid-width--small">
+        <Badge href="#" className="margin-s bg--bluedark animation-fogwave">Bandekriminialitet</Badge>
+        <Badge href="#" className="margin-s bg--green animation-fogwave">Sport</Badge>
+        <Badge href="#" className="margin-s bg--greendark animation-fogwave">Nicklas Bendtner</Badge>
+      </div>
+    </PillContent>
+    <PillContent>
+      <Prism
+        language="html"
+        source={`<ArticleCard
+          className="animation-fogwave"
+          href="{article.href}"
+          media="{{src:'https://via.placeholder.com/610x343&text=610x343'}}"
+          section="{article.section}"
+          timestamp="{article.timestamp}"
+          title="{article.title}"
+          />`}
+      />
+      <Prism
+        language="html"
+        source={`<Badge href="#" className="margin-s bg--greendark animation-fogwave">Nicklas Bendtner</Badge>`}
+      />
+      <Prism
+        language="html"
+        source={`<a href="#" class="badge badge--primary animation-fogwave margin-s--r">Bandekriminialitet</a>`}
+      />
+    </PillContent>
+  </PillNavigation>
 
   <h3>Overblik over animationer</h3>
   <div class="grid-width--large">
@@ -35,50 +86,4 @@
       </div>
     </div>
   </div>
-  <h3>Eksempler på animationer</h3>
-</div>
-
-<div class="grid-width--large">
-<PillNavigation>
-  <div class="flex flex-justify--end width-1of1">
-    <PillList>
-      <Pill><i class="fas fa-cubes" /></Pill>
-      <Pill><i class="fas fa-code" /></Pill>
-    </PillList>
-  </div>
-  <PillContent>
-    <div class="flex grid-width--small">
-      <ArticleCard
-        className="animation-fogwave"
-        href={article.href}
-        media={{ src: 'https://via.placeholder.com/610x343&text=610x343' }}
-        section={article.section}
-        timestamp={article.timestamp}
-        title={article.title}
-      />
-    </div>
-    <div class="flex grid-width--small">
-      <Badge href="#" className="margin-s bg--bluedark animation-fogwave">Bandekriminialitet</Badge>
-      <Badge href="#" className="margin-s bg--green animation-fogwave">Sport</Badge>
-      <Badge href="#" className="margin-s bg--greendark animation-fogwave">Nicklas Bendtner</Badge>
-    </div>
-  </PillContent>
-  <PillContent>
-    <Prism
-      language="html"
-      source={`<ArticleCard
-      className="animation-fogwave"
-      href="{article.href}"
-      media="{{src:'https://via.placeholder.com/610x343&text=610x343'}}"
-      section="{article.section}"
-      timestamp="{article.timestamp}"
-      title="{article.title}"
-      />`}
-    />
-    <Prism
-      language="html"
-      source={`<Badge href="#" className="margin-s bg--greendark animation-fogwave">Nicklas Bendtner</Badge>`}
-    />
-  </PillContent>
-</PillNavigation>
 </div>
