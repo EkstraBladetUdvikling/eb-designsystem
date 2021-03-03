c3410685aaa57e97f93a7995c826cf5f35dd56ee
tegner <jesper.h.tegner@jppol.dk>
2021-03-03 21:27:53 +0100

    Added icons to lib

--- documentation/components/Icon.svelte
+++ documentation/components/Icon.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
   import Prism from 'svelte-prism';
 
-  import Icon, { svgIcons } from '../../src/_components/icon';
+  import Icon, { icons } from '../../src/_components/icon/Icon.svelte';
 </script>
 
 <div class="grid-width--small">
   <h1 class="color--eb">Icon library</h1>
   <p>Der findes følgende svg ikoner</p>
-  <ul>
-    {#each svgIcons as name}
-      <li>{name}</li>
-    {/each}
-  </ul>
+
   <div class="flex flex-wrap--wrap">
-    {#each svgIcons as name}
+    {#each Object.entries(icons) as [name]}
       <div
         class="flex flex-column flex-align--center flex-justify--center margin-m padding-m"
         style="border: 1px solid #111; border-radius: 5px;"
---
+++ src/_components/icon/svgs/ebplus_sort.svelte
@@ -0,0 +1,17 @@
+<svg
+  version="1.1"
+  id="Layer_1"
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  x="0px"
+  y="0px"
+  viewBox="0 122.1 595.3 597.4"
+  enable-background="new 0 122.1 595.3 597.4"
+  xml:space="preserve"
+>
+  <g>
+    <g>
+      <path d="M237.4,122.1h120.5v238.5h237.4V481H357.9v238.5H237.4V481H0V360.5h237.4V122.1z" />
+    </g>
+  </g>
+</svg>
