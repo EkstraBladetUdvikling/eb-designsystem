942aace93e3e49de2f02d3d3659abd81f0f33401
tegner <jesper.h.tegner@jppol.dk>
2021-03-05 08:28:18 +0100

    Svelte icons (#70)
    * Added icons to lib
    
    * back to previous icon
    
    * icon doc

--- documentation/components/Icon.svelte
+++ documentation/components/Icon.svelte
@@ -1,19 +1,43 @@
 <script lang="ts">
   import Prism from 'svelte-prism';
 
-  import Icon, { svgIcons } from '../../src/_components/icon';
+  import Icon, { IconTypes } from '../../src/_components/icon/Icon.svelte';
+  const icons: IconTypes[] = [
+    'angledown',
+    'angleleft',
+    'angleright',
+    'angleup',
+    'article',
+    'check',
+    'creditcard',
+    'ebplusicon',
+    'ebplussort',
+    'envelope',
+    'gallery',
+    'headphones',
+    'headset',
+    'lock',
+    'medielogin',
+    'menubars',
+    'mitebregular',
+    'mitebsolid',
+    'mobilphone',
+    'newspaper',
+    'playcircle',
+    'tagregular',
+    'tagsolid',
+    'tagsregular',
+    'tagssolid',
+    'video',
+  ];
 </script>
 
 <div class="grid-width--small">
   <h1 class="color--eb">Icon library</h1>
   <p>Der findes følgende svg ikoner</p>
-  <ul>
-    {#each svgIcons as name}
-      <li>{name}</li>
-    {/each}
-  </ul>
+
   <div class="flex flex-wrap--wrap">
-    {#each svgIcons as name}
+    {#each icons as name}
       <div
         class="flex flex-column flex-align--center flex-justify--center margin-m padding-m"
         style="border: 1px solid #111; border-radius: 5px;"
