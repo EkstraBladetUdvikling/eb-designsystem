96a9479f2636401ad7c0233e4b657828ea8ab774
tegner <jesper.h.tegner@jppol.dk>
2021-03-09 08:58:59 +0100

    font

---
+++ src/_utilities/fonts/fontsizes.ts
@@ -0,0 +1,113 @@
+export const fontsizes = {
+  xxxlarge: {
+    desktop: {
+      xxxlarge: 50,
+    },
+
+    smartphone: {
+      xxxlarge: 36,
+    },
+
+    tablet: {
+      xxxlarge: 50,
+    },
+  },
+
+  xxlarge: {
+    desktop: {
+      xxlarge: 30,
+    },
+
+    smartphone: {
+      xxlarge: 30,
+    },
+
+    tablet: {
+      xxlarge: 30,
+    },
+  },
+
+  xlarge: {
+    desktop: {
+      xlarge: 20,
+    },
+
+    smartphone: {
+      xlarge: 20,
+    },
+
+    tablet: {
+      xlarge: 20,
+    },
+  },
+
+  large: {
+    desktop: {
+      large: 18,
+    },
+
+    smartphone: {
+      large: 15,
+    },
+
+    tablet: {
+      large: 18,
+    },
+  },
+
+  medium: {
+    desktop: {
+      medium: 16,
+    },
+
+    smartphone: {
+      medium: 15,
+    },
+
+    tablet: {
+      medium: 16,
+    },
+  },
+
+  small: {
+    desktop: {
+      small: 14,
+    },
+
+    smartphone: {
+      small: 12,
+    },
+
+    tablet: {
+      small: 14,
+    },
+  },
+
+  xsmall: {
+    desktop: {
+      xsmall: 12,
+    },
+
+    smartphone: {
+      xsmall: 10,
+    },
+
+    tablet: {
+      xsmall: 12,
+    },
+  },
+
+  xxsmall: {
+    desktop: {
+      xxsmall: 10,
+    },
+
+    smartphone: {
+      xxsmall: 7,
+    },
+
+    tablet: {
+      xxsmall: 10,
+    },
+  },
+};
