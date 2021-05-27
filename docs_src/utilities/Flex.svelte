97a4642e691f0c9119c5e46026bdc028947cfa7d
Thomas Carlsen <thomascarlsen86@gmail.com>
2021-05-27 10:11:35 +0200

    flex

--- docs_src/utilities/Flex.svelte
+++ docs_src/utilities/Flex.svelte
@@ -1,181 +1,223 @@
 <script>
   import Prism from 'svelte-prism';
+  import { Card } from '../../src';
 </script>
 
-<div class="grid-width--large">
-  <h1 class="color--eb">Flex</h1>
-  <h3>Anvendelse af Flex</h3>
-  <p>
-    Flex består af forskellige CSS klasser, som både kan anvendes på komponenter og elementer ved tilføjelse af class.
-  </p>
-  <p>HTML element</p>
-  <Prism language="html" source={`<div class="flex"></div>`} />
-  <p>Svelte component</p>
-  <Prism language="html" source={`<component className="flex"></component>`} />
+<h1 class="color--eb">Flex</h1>
 
-  <h3>Flex container</h3>
-  <p>
-    Flex tilføjer <code>display: flex</code> til container element og transformere alle child elementer til flex-items.
-  </p>
-  <Prism language="html" source={`<div class="flex">I'm a flexbox container!</div>`} />
-  <div class="flex bg--graa7 padding-l">I'm a flexbox container!</div>
+<h3>Flex container</h3>
 
-  <h3>Flex center</h3>
-  <p>Flex center centrere alle child elementer både horizontalt og vertical.</p>
-  <Prism language="html" source={`<div class="flex flex--center">...</div>`} />
-  <div class="flex flex--center bg--graa7" style="height: 100px;">
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-  </div>
+<Prism language="html">
+  {`<div class="flex"></div>`}
+</Prism>
 
-  <h3>Flex justify content</h3>
-  <p>Justify content anvendes til <i>horizontal</i> placering af child elementer i flex containers.</p>
-  <Prism language="html" source={`<div class="flex flex-justify--start">...</div>`} />
-  <div class="flex flex-justify--start bg--graa7">
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-  </div>
-  <Prism language="html" source={`<div class="flex flex-justify--center">...</div>`} />
-  <div class="flex flex-justify--center bg--graa7">
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-  </div>
-  <Prism language="html" source={`<div class="flex flex-justify--end">...</div>`} />
-  <div class="flex flex-justify--end bg--graa7">
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-  </div>
-  <Prism language="html" source={`<div class="flex flex-justify--around">...</div>`} />
-  <div class="flex flex-justify--around bg--graa7">
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-  </div>
-  <Prism language="html" source={`<div class="flex flex-justify--between">...</div>`} />
-  <div class="flex flex-justify--between bg--graa7">
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-  </div>
+<h3>Justify content</h3>
+<p>Justify content anvendes til <i>horizontal</i> placering af child elementer.</p>
 
-  <h3>Flex align items</h3>
-  <p>Align items anvendes til <i>veritcal</i> placering af child elementer i flex containers.</p>
-  <p>Individuelle child elementer(flex-item) kan også vertical placeres med <code>flex-item--start | center | end | strech</code></p>
-  <Prism language="html" source={`<div class="flex flex-align--start">...</div>`} />
-  <div class="flex flex-align--start bg--graa7" style="height: 100px;">
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
+<Card className="bg--graa7">
+  <strong class="margin-m">flex flex-justify--start</strong>
+  <div class="flex flex-justify--start bg--eb">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
   </div>
-  <Prism language="html" source={`<div class="flex flex-align--center">...</div>`} />
-  <div class="flex flex-align--center bg--graa7" style="height: 100px;">
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
+  <strong class="margin-m margin-l--t">flex flex-justify--end</strong>
+  <div class="flex flex-justify--end bg--eb">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
   </div>
-  <Prism language="html" source={`<div class="flex flex-align--end">...</div>`} />
-  <div class="flex flex-align--end bg--graa7" style="height: 100px;">
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
+  <strong class="margin-m margin-l--t">flex flex-justify--center</strong>
+  <div class="flex flex-justify--center bg--eb">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
   </div>
-  <Prism language="html" source={`<div class="flex flex-align--strech">...</div>`} />
-  <div class="flex flex-align--strech bg--graa7" style="height: 100px;">
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item</div>
+  <strong class="margin-m margin-l--t">flex flex-justify--around</strong>
+  <div class="flex flex-justify--around bg--eb">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
   </div>
+  <strong class="margin-m margin-l--t">flex flex-justify--between</strong>
+  <div class="flex flex-justify--between bg--eb">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
+  </div>
+</Card>
 
-  <h3>Flex directions</h3>
-  <p>Flex directions bestemmer rækkefølgen for visning af child elementer i flex containeren.</p>
-  <Prism language="html" source={`<div class="flex flex-column">...</div>`} />
-  <div class="flex flex-column bg--graa7">
-    <div class="flex-item bg--graa5 padding-l">Flex item 1</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 2</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 3</div>
-  </div>
-  <Prism language="html" source={`<div class="flex flex-column--reverse">...</div>`} />
-  <div class="flex flex-column--reverse bg--graa7">
-    <div class="flex-item bg--graa5 padding-l">Flex item 1</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 2</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 3</div>
-  </div>
-  <Prism language="html" source={`<div class="flex flex-row--reverse">...</div>`} />
-  <div class="flex flex-row--reverse bg--graa7">
-    <div class="flex-item bg--graa5 padding-l">Flex item 1</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 2</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 3</div>
-  </div>
+<Prism language="html">
+  {`<div class="flex flex-justify--start"></div>
+<div class="flex flex-justify--end"></div>
+<div class="flex flex-justify--center"></div>
+<div class="flex flex-justify--around"></div>
+<div class="flex flex-justify--between"></div>`}
+</Prism>
 
-  <h3>Flex wrap</h3>
-  <p>
-    Flex wrap sørger for at alle child elementer beholder deres størrelse og istedet wrapper elementerne på multiple lines
-    istedet for one-line.
-  </p>
-  <p>
-    Ex. browser-default er <code>flex-wrap: nowrap;</code> som forcer alle child elementer til at stå på samme line ved at squeeze
-    dem sammen.
-  </p>
+<h3>Align items</h3>
+<p>Align items anvendes til <i>veritcal</i> placering af child elementer.</p>
 
-  <Prism language="html" source={`<div class="flex">...</div>`} />
-    <div class="flex bg--graa7">
-      <div class="flex-item bg--graa5 padding-l">Flex item 1</div>
-      <div class="flex-item bg--graa5 padding-l">Flex item 2</div>
-      <div class="flex-item bg--graa5 padding-l">Flex item 3</div>
-      <div class="flex-item bg--graa5 padding-l">Flex item 4</div>
-      <div class="flex-item bg--graa5 padding-l">Flex item 5</div>
-      <div class="flex-item bg--graa5 padding-l">Flex item 6</div>
-      <div class="flex-item bg--graa5 padding-l">Flex item 7</div>
-      <div class="flex-item bg--graa5 padding-l">Flex item 8</div>
-      <div class="flex-item bg--graa5 padding-l">Flex item 9</div>
-    </div>
-
-  <Prism language="html" source={`<div class="flex flex-wrap--wrap">...</div>`} />
-  <div class="flex flex-wrap--wrap bg--graa7">
-    <div class="flex-item bg--graa5 padding-l">Flex item 1</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 2</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 3</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 4</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 5</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 6</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 7</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 8</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item 9</div>
+<Card className="bg--graa7">
+  <strong class="margin-m">flex flex-align--start</strong>
+  <div class="flex flex-align--start bg--eb" style="height: 100px">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
   </div>
-
-  <h3>Flex size</h3>
-  <p>Flex size bestemmer hvordan størrelsen på child elementer skal opføre sig.</p>
-  <p>
-    <code>flex-item--noshrink</code> sørger for at et child element altid vil have den samme størrelse også på scalering.
-  </p>
-  <Prism
-    language="html"
-    source={`<div class="flex"><div class="flex-item flex-item--noshrink">Flex item no-shrink</div></div>`}
-  />
-  <div class="flex bg--graa7">
-    <div class="flex-item bg--graa5 padding-l">Flex item normal</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item normal</div>
-    <div class="flex-item flex-item--noshrink bg--graa5 padding-l">Flex item no-shrink</div>
-    <div class="flex-item flex-item--noshrink bg--graa5 padding-l">Flex item no-shrink</div>
-    <div class="flex-item flex-item--noshrink bg--graa5 padding-l">Flex item no-shrink</div>
-    <div class="flex-item flex-item--noshrink bg--graa5 padding-l">Flex item no-shrink</div>
-    <div class="flex-item flex-item--noshrink bg--graa5 padding-l">Flex item no-shrink</div>
+  <strong class="margin-m margin-l--t">flex flex-align--end</strong>
+  <div class="flex flex-align--end bg--eb" style="height: 100px">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
   </div>
-
-  <p>
-    <code>flex-item--grow</code> sørger for at child element udfylder den tilbageværende plads i flex containeren.
-  </p>
-  <Prism
-    language="html"
-    source={`<div class="flex"><div class="flex-item flex-item--grow">Flex item grow</div></div>`}
-  />
-  <div class="flex bg--graa7">
-    <div class="flex-item bg--graa5 padding-l">Flex item normal</div>
-    <div class="flex-item flex-item--grow bg--graa5 padding-l">Flex item grow</div>
-    <div class="flex-item bg--graa5 padding-l">Flex item normal</div>
+  <strong class="margin-m margin-l--t">flex flex-align--center</strong>
+  <div class="flex flex-align--center bg--eb" style="height: 100px">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
   </div>
-</div>
+  <strong class="margin-m margin-l--t">flex flex-align--strech</strong>
+  <div class="flex flex-align--strech bg--eb" style="height: 100px">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
+  </div>
+</Card>
+
+<Prism language="html">
+  {`<div class="flex flex-align--start"></div>
+<div class="flex flex-align--end"></div>
+<div class="flex flex-align--center"></div>
+<div class="flex flex-align--strech"></div>`}
+</Prism>
+
+<h3>Flex center</h3>
+<p>Flex center centrere alle child elementer både <i>horizontalt</i> og <i>vertical</i>.</p>
+
+<Card className="bg--graa7">
+  <div class="flex flex--center bg--eb" style="height: 100px">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
+  </div>
+</Card>
+
+<Prism language="html">
+  {`<div class="flex flex--center"></div>`}
+</Prism>
+
+<h3>Directions</h3>
+<p>Directions bestemmer rækkefølgen for visning af child elementer.</p>
+
+<Card className="bg--graa7">
+  <strong class="margin-m">flex flex-column</strong>
+  <div class="flex flex-column">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
+  </div>
+  <strong class="margin-m margin-l--t">flex flex-column--reverse</strong>
+  <div class="flex flex-column--reverse">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
+  </div>
+  <strong class="margin-m margin-l--t">flex</strong>
+  <div class="flex bg--eb">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
+  </div>
+  <strong class="margin-m margin-l--t">flex flex-row--reverse</strong>
+  <div class="flex flex-row--reverse bg--eb">
+    <div class="bg--graa4 padding-l">Flex item 1</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
+  </div>
+</Card>
+
+<Prism language="html">
+  {`<div class="flex flex-column"></div>
+<div class="flex flex-column--reverse"></div>
+<div class="flex"></div>
+<div class="flex flex-row--reverse"></div>`}
+</Prism>
+
+<h3>Wrap</h3>
+<p>
+  Flex wrap sørger for at alle child elementer beholder deres størrelse og istedet wrapper elementerne på multiple lines
+  istedet for one-line.
+</p>
+
+<Card className="bg--graa7">
+  <strong class="margin-m">flex</strong>
+  <div class="flex">
+    <div class="bg--graa4 width-1of3 padding-l">Flex item 1</div>
+    <div class="bg--graa3 width-1of3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 width-1of3 padding-l">Flex item 3</div>
+    <div class="bg--graa1 width-1of3 padding-l">Flex item 4</div>
+  </div>
+  <strong class="margin-m margin-l--t">flex flex-wrap--wrap</strong>
+  <div class="flex flex-wrap--wrap">
+    <div class="bg--graa4 width-1of3 padding-l">Flex item 1</div>
+    <div class="bg--graa3 width-1of3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 width-1of3 padding-l">Flex item 3</div>
+    <div class="bg--graa1 width-1of3 padding-l">Flex item 4</div>
+  </div>
+</Card>
+
+<Prism language="html">
+  {`<div class="flex"></div>
+<div class="flex flex-wrap--wrap"></div>`}
+</Prism>
+
+<h2>Flex items</h2>
+
+<h3>Align self</h3>
+
+<Card className="bg--graa7">
+  <div class="flex bg--eb" style="height: 100px">
+    <div class="flex-item--start bg--graa4 width-1of3 padding-l">flex-item--start</div>
+    <div class="flex-item--end bg--graa2 width-1of3 padding-l">flex-item--end</div>
+    <div class="flex-item--center bg--graa3 width-1of3 padding-l">flex-item--center</div>
+    <div class="flex-item--stretch bg--graa1 width-1of3 padding-l">flex-item--stretch</div>
+  </div>
+</Card>
+
+<Prism language="html">
+  {`<div class="flex">
+  <div class="flex-item--start"></div>
+  <div class="flex-item--end"></div>
+  <div class="flex-item--center"></div>
+  <div class="flex-item--stretch"></div>
+</div>`}
+</Prism>
+
+<h3>Grow and shrik</h3>
+<p><code>flex-item--grow</code> sørger for at child element udfylder den tilbageværende plads i flex containeren.</p>
+<p>
+  <code>flex-item--noshrink</code> sørger for at et child element altid vil have den samme størrelse også på scalering.
+</p>
+
+<Card className="bg--graa7">
+  <strong class="margin-m margin-l--t">Grow</strong>
+  <div class="flex bg--eb">
+    <div class="flex-item--grow bg--graa4 padding-l">flex-item--grow</div>
+    <div class="bg--graa3 padding-l">Flex item 2</div>
+    <div class="bg--graa2 padding-l">Flex item 3</div>
+  </div>
+  <strong class="margin-m margin-l--t">No shrink</strong>
+  <div class="flex bg--eb">
+    <div class="flex-item--noshrink width-2of3 bg--graa4 padding-l">flex-item--noshrink width-2of3</div>
+    <div class="bg--graa3 width-1of3 padding-l">width-1of3</div>
+    <div class="bg--graa2 width-1of3 padding-l">width-1of3</div>
+  </div>
+</Card>
+
+<Prism language="html">
+  {`<div class="flex">
+  <div class="flex-item--grow"></div>
+  <div class="flex-item--noshrink"></div>
+</div>`}
+</Prism>
