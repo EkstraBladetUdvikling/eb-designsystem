ab0b28eb63ee6fd5ae56a0bed66f022689b7abcc
Thomas Carlsen <thomascarlsen86@gmail.com>
2021-05-28 10:58:25 +0200

    source toogle demo on tooltip

--- docs_src/components/Tooltip.svelte
+++ docs_src/components/Tooltip.svelte
@@ -1,55 +1,92 @@
 <script>
   import Prism from 'svelte-prism';
-
+  import { sourceType } from '../stores';
   import { Tooltip } from '../../src';
+
+  let source = '';
+
+  sourceType.subscribe((value) => {
+    source = value;
+  });
 </script>
 
 <h1 class="color--eb">Tooltip</h1>
-<div class="grid-width--small">
-  <Prism language="js" source={`import { Tooltip } from '@ekstra-bladet/designsystem';`} />
 
-  <h2>Default tooltip</h2>
-  <div class="flex margin-l--tb">
-    <Tooltip>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <p>Vestibulum nec felis viverra, aliquam erat vel, ornare ipsum.</p>
-      <p>Aenean a blandit lacus, sed faucibus ante.</p>
-    </Tooltip>
-  </div>
+{#if source === 'svelte'}
+  <Prism language="js">
+    {`import { Tooltip } from '@ekstra-bladet/designsystem';`}
+  </Prism>
+{/if}
 
-  <Prism
-    language="html"
-    source={`
-      <Tooltip>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <p>Vestibulum nec felis viverra, aliquam erat vel, ornare ipsum.</p>
-        <p>Aenean a blandit lacus, sed faucibus ante.</p>
-      </Tooltip>
-    `}
-  />
+<h3>Default tooltip</h3>
 
-  <h2>Tooltip i højre side</h2>
-
-  <div class="flex flex-justify--between grid-width--small margin-l--tb">
-    <h3 class="flex-item">Flot overskrift</h3>
-    <Tooltip className="flex-item flex-item--center" position="right">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <p>Vestibulum nec felis viverra, aliquam erat vel, ornare ipsum.</p>
-      <p>Aenean a blandit lacus, sed faucibus ante.</p>
-    </Tooltip>
-  </div>
-
-  <Prism
-    language="html"
-    source={`
-      <div class="flex flex-justify--between grid-width--small">
-        <h3 class="flex-item">Flot overskrift</h3>
-        <Tooltip className="flex-item flex-item--center" position="right">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <p>Vestibulum nec felis viverra, aliquam erat vel, ornare ipsum.</p>
-          <p>Aenean a blandit lacus, sed faucibus ante.</p>
-        </Tooltip>
-      </div>
-    `}
-  />
+<div class="flex margin-l--tb">
+  <Tooltip>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <p>Vestibulum nec felis viverra, aliquam erat vel, ornare ipsum.</p>
+    <p>Aenean a blandit lacus, sed faucibus ante.</p>
+  </Tooltip>
 </div>
+
+{#if source === 'svelte'}
+  <Prism language="html">
+    {`<Tooltip>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+</Tooltip>`}
+  </Prism>
+{:else}
+  <Prism language="html">
+    {`<label class="tooltip">
+  <input type="checkbox" hidden class="tooltip-input"/>
+  <div class="tooltip-off">
+    <i class="tooltip-toggle fas fa-question"/>
+  </div>
+  <div class="tooltip-on">
+    <i class="tooltip-toggle fas fa-times"/>
+    <div class="padding-s">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  </div>
+</label>`}
+  </Prism>
+{/if}
+
+<h3>Tooltip i højre side</h3>
+
+<div class="flex flex-justify--between grid-width--small margin-l--tb">
+  <h3>Header</h3>
+  <Tooltip position="right" className="flex-item--center">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <p>Vestibulum nec felis viverra, aliquam erat vel, ornare ipsum.</p>
+    <p>Aenean a blandit lacus, sed faucibus ante.</p>
+  </Tooltip>
+</div>
+
+{#if source === 'svelte'}
+  <Prism language="html">
+    {`<div class="flex flex-justify--between">
+  <h3>Header</h3>
+  <Tooltip position="right" className="flex-item--center">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  </Tooltip>
+</div>`}
+  </Prism>
+{:else}
+  <Prism language="html">
+    {`<div class="flex flex-justify--between">
+  <h3>Header</h3>
+  <label class="tooltip tooltip--right flex-item--center">
+    <input type="checkbox" hidden class="tooltip-input"/>
+    <div class="tooltip-off">
+      <i class="tooltip-toggle fas fa-question"/>
+    </div>
+    <div class="tooltip-on">
+      <i class="tooltip-toggle fas fa-times"/>
+      <div class="padding-s">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+  </label>
+</div>`}
+  </Prism>
+{/if}
