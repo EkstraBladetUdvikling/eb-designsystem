326e64a9a15c896bfbc1caaf8ea1ab79e22e84b5
Thomas Carlsen <thomascarlsen86@gmail.com>
2021-05-31 15:10:44 +0200

    source toggle for articlecard

--- docs_src/components/ArticleCard.svelte
+++ docs_src/components/ArticleCard.svelte
@@ -1,172 +1,88 @@
 <script lang="ts">
   import Prism from 'svelte-prism';
-
+  import { LoremIpsum } from 'lorem-ipsum';
+  import { sourceType } from '../stores';
   import { ArticleCard } from '../../src';
 
-  let article = {
-    href: '#',
-    media: {
-      src: 'https://via.placeholder.com/610x343&text=610x343',
+  const lorem = new LoremIpsum({
+    wordsPerSentence: {
+      max: 12,
+      min: 4,
     },
-    section: 'Sport',
-    colorClass: 'sport',
-    timestamp: 'Thu Mar 18 2021 20:46:32',
-    title: 'List element',
-  };
+  });
 
-  let article1 = {
-    href: '#',
-    media: {
-      src: 'https://via.placeholder.com/610x343&text=610x343',
-    },
-    section: 'Underholdning',
-    colorClass: 'underholdning',
-    timestamp: 'Thu Mar 28 2021 20:46:32',
-    title: 'List element',
-  };
+  let source = '';
 
-  let article2 = {
-    href: '#',
-    isPlus: true,
-    media: {
-      src: 'https://via.placeholder.com/610x343&text=610x343',
-    },
-    section: 'Udenlandsk fodbold',
-    colorClass: 'sport',
-    timestamp: 'Thu Mar 31 2021 20:46:32',
-    title: `Sag om rockervold: 'Når han er på stoffer, siger han ting, der ikke passer'`,
-  };
-  document.addEventListener(
-    'articleCardInview',
-    function (e) {
-      console.log('articleCardInview', e);
-    },
-    false
-  );
+  sourceType.subscribe((value) => {
+    source = value;
+  });
+
+  function rdmArticleData(mediaWidth = 640, mediaHeight = 360) {
+    const colorClasses = ['sport', 'sex-samliv', 'forbrug', 'underholdning', 'flash', 'nyheder'];
+    const article = {
+      href: '#',
+      isPlus: Math.random() < 0.3,
+      isBreaking: Math.random() < 0.1,
+      media: {
+        src: `https://loremflickr.com/${mediaWidth}/${mediaHeight}/city,people,nature,animal?random=${Math.random()}`,
+      },
+      section: lorem.generateWords(1),
+      colorClass: colorClasses[Math.floor(Math.random() * colorClasses.length)],
+      timestamp: randomDate(),
+      title: lorem.generateSentences(1),
+      truncateTitle: false,
+    };
+
+    return article;
+  }
+
+  function randomDate() {
+    const start = new Date(2019, 0, 1);
+    const end = new Date();
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  }
 </script>
 
-<div class="grid-width--small">
-  <h1 class="color--eb">ArticleCard</h1>
-  <Prism
-    language="js"
-    source={`
-    import { ArticleCard } from '@ekstra-bladet/designsystem';
-    `}
-  />
-  <p>ArticleCard attributer</p>
-  <Prism
-    language="js"
-    source={`
-    interface IMediaOptions {
-      className: string;
-      height: string;
-      src: string;
-      width: string;
-    }
+<h1 class="color--eb">Article Card</h1>
 
-    export let className: string = undefined;
-    export let colorClass: string = undefined;
-    export let href: string = undefined;
-    export let loading: boolean = false;
-    export let isBreaking: boolean = false;
-    export let isPlus: boolean = false;
-    export let media: Partial<IMediaOptions> = undefined;
-    export let maxLines: number = undefined;
-    export let section: string = undefined;
-    export let style: string = undefined;
-    export let theme: TThemes = undefined;
-    export let timestamp: string = undefined;
-    export let title: string;
-    export let type: TCardType = undefined;
-  `}
-  />
+{#if source === 'svelte'}
+  <Prism language="js">
+    {`import { ArticleCard } from '@ekstra-bladet/designsystem';`}
+  </Prism>
 
-  <ArticleCard {...article} intersection={true} />
+  <ArticleCard {...rdmArticleData()} />
 
-  <ArticleCard {...article1} />
+  <Prism language="html">
+    {`<ArticleCard {...article} />`}
+  </Prism>
 
-  <ArticleCard {...article2} />
+  <h3>Small-media</h3>
 
-  <Prism
-    language="js"
-    source={`
-    let article = {
-      href: '#',
-      media: {
-        src: 'https://via.placeholder.com/610x343&text=610x343',
-      },
-      section: 'Sport',
-      colorClass: 'sport',
-      timestamp: 'Thu Mar 18 2021 20:46:32',
-      title: 'List element',
-    };
-    `}
-  />
-  <ArticleCard
-    colorClass={article.colorClass}
-    href={article.href}
-    media={undefined}
-    section={article.section}
-    timestamp={article.timestamp}
-    title={article.title}
-  />
-  <Prism
-    language="html"
-    source={`
-    <ArticleCard href="{article.href}" media="{{src:'https://via.placeholder.com/610x343&text=610x343'}}" section="{article.section}" timestamp="{article.timestamp}" title="{article.title}" />
-    <ArticleCard {...article} />
-  `}
-  />
+  <ArticleCard className="margin-m--b" type="small-media" {...rdmArticleData(250, 120)} />
+  <ArticleCard type="small-media--reverse" {...rdmArticleData(250, 120)} />
 
-  <ArticleCard {...article} title="Small media card list element" type="small-media" />
-  <Prism
-    language="html"
-    source={`
-    <ArticleCard {...article} title="Small media card list element" type="small-media" />
-  `}
-  />
+  <Prism language="html">
+    {`<ArticleCard className="small-media" {...article} />
+<ArticleCard className="small-media--reverse" {...article} />`}
+  </Prism>
 
-  <ArticleCard
-    {...article}
-    maxLines={2}
-    media={{
-      height: '115',
-      src: 'https://via.placeholder.com/200x112&text=200x112',
-      width: '200',
-    }}
-    title="Small media reverse card list element"
-    type="small-media--reverse"
-  />
-  <Prism
-    language="html"
-    source={`
-    <ArticleCard
-      {...article}
-      maxLines={2}
-      media="{{
-        height: '115',
-        src: 'https://via.placeholder.com/200x112&text=200x112',
-        width: '200',
-      }}"
-      title="Small media reverse card list element"
-      type="small-media--reverse" />
-  `}
-  />
-
-  <h2>Loading placeholder</h2>
+  <h3>Loading placeholder</h3>
 
   <ArticleCard loading={true} />
   <ArticleCard loading={true} type="small-media" />
   <ArticleCard loading={true} type="small-media--reverse" />
 
-  <Prism
-    language="html"
-    source={`
-      <ArticleCard loading={true} />
-      <ArticleCard loading={true} type="small-media" />
-      <ArticleCard loading={true} type="small-media--reverse" />
-    `}
-  />
+  <Prism language="html">
+    {`<ArticleCard loading={true} />
+<ArticleCard loading={true} type="small-media" />
+<ArticleCard loading={true} type="small-media--reverse" />`}
+  </Prism>
 
-  <ArticleCard isPlus={true} {...article} style="width: 215px;" />
-</div>
+  <h3>Intersection</h3>
+
+  <Prism language="html">
+    {`<ArticleCard {...article} intersection={true} />`}
+  </Prism>
+{:else}
+  <p>ArticleCard er en ren Svelte component.</p>
+{/if}
