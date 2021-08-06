aed2c952eefea92e4e16170855455738671dc47c
Mikkel B. Jensen <mikkel.jensen@eb.dk>
2021-08-06 12:43:24 +0200

    translate.google.com

--- docs_src/guidelines/Svelte.svelte
+++ docs_src/guidelines/Svelte.svelte
@@ -1,32 +1,23 @@
 <h1 class="color--eb">Svelte</h1>
 
 <ol>
+  <li>There must be a rigorous division of logic (script-part) and views (template, class selection, etc.)</li>
   <li>
-    Der skal være en stringent opdeling af logik (hvad kan den - script-tagget, datahåndtering - hvis dette ikke kan
-    løse stateforbindelse) og visning (template, udvælgelse af classer etc.)
+    Component-specific styling should preferably be solved using the designsystem instead of styling within the
+    Svelte-files.
   </li>
+  <li>Svelte functions should (if possible) be used instead of native functions (eg createEventListener).</li>
+  <li>Script tag content must be kept to an absolute minimum.</li>
   <li>
-    Som udgangspunkt laves der ikke komponentspecifik styling i Svelte-regi - der benyttes classes fra designsystemet.
+    As a rule, the state must provide all data in the format required by the components. Ie. Filtering takes place in
+    state management and not in the respective components.
   </li>
+  <li>Design system: on:click should exist on all elements of the design system</li>
+  <li>The use of Svelte is agreed in the front-end group, so that it is only used where it makes sense.</li>
   <li>
-    Der skal som udgangspunkt benyttes Svelte-funktioner fremfor native funktioner (f.eks. createEventListener).&nbsp;
+    Svelte-store is used and each part of the state is divided into several readable / writeable, instead of one object.
   </li>
-  <li>Script-tags indhold skal holdes til et absolut minimum udover imports og exports.&nbsp;</li>
-  <li>
-    Som udgangspunkt skal state levere al data I det format som komponenterne kræver. Dvs. Filtrering sker i
-    state-håndtering og ikke I de respektive komponenter.&nbsp;
-  </li>
-  <li>
-    Designsystem: Der arbejdes grundkomponenter og higher-order-komponenter, der arver grundkomponenter og har mere
-    specifikke brugsmuligheder.
-  </li>
-  <li>Designsystem: on:click bindes på alle elementer i designsystemet</li>
-  <li>Benyttelse af Svelte aftales i frontend-gruppen, så det kun bruges hvor det giver mening.</li>
-  <li>Svelte-store benyttes og hver del af staten opdeles i flere readable/writeable, i stedet for et object.</li>
-  <li>
-    Writables må kun opdateres vha. actions og må ikke exporteres direkte. Der udstilles en samlet derived state
-    (readable) og actions.
-  </li>
+  <li>Writables may only be updated using actions (functions) and may not be exported directly.</li>
 </ol>
 
 <style>
