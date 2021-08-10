5796965de448b5837c3c99d1fb78b80cdb3a821f
tegner <jesper.h.tegner@jppol.dk>
2021-08-10 12:50:13 +0200

    more cleanup and added utilities to dist

--- node/build-css.js
+++ node/build-css.js
@@ -1,20 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
-const pkg = require('../package.json');
-
 const postcss = require('postcss');
+const { getOutputFileNames } = require('typescript');
 const postcssImport = require('postcss-import')({
   skipDuplicates: true,
 });
 const discardDuplicates = require('postcss-discard-duplicates')();
 const discardUnused = require('postcss-discard-unused')();
 const mergeRules = require('postcss-merge-rules')();
-const postcssCustomMedia = require('postcss-custom-media');
-const presetEnv = require('postcss-preset-env');
 const runtimeArguments = process.argv.slice(2);
-
-const TEMPVERSION = './.tempversion';
 
 const importFrom = [
   './node_modules/@ekstra-bladet/eb-colors/dist/eb-colors-vars-rgb.css',
@@ -22,22 +17,6 @@ const importFrom = [
   './src/_variables.css',
   './src/_custom-mediaqueries.css',
 ];
-
-const cssnextObject = {
-  browsers: 'ie 11',
-  features: {
-    'nesting-rules': true,
-  },
-  importFrom,
-  preserve: false,
-  stage: 0,
-  warnForDuplicates: false,
-};
-
-const customMediaOptions = {
-  importFrom: ['./src/_custom-mediaqueries.css'],
-  preserve: false,
-};
 
 const readFolder = (folderName, filesToFind, array, lvl = 0) => {
   fs.readdirSync(folderName, { withFileTypes: true }).forEach((output) => {
@@ -56,17 +35,10 @@ const readFolder = (folderName, filesToFind, array, lvl = 0) => {
 const getOptions = (args) => {
   const options = {
     build: '',
-    watching: false,
   };
   args.forEach((arg) => {
     if (arg.indexOf('--build') !== -1) {
-      options.build = `--${arg.split('--build=')[1]}`;
-    }
-    if (arg.indexOf('--folder') !== -1) {
-      options.folder = arg.split('--folder=')[1];
-    }
-    if (arg.indexOf('--watcher') !== -1) {
-      options.build = arg;
+      options.build = arg.split('--build=')[1];
     }
   });
   return options;
@@ -76,12 +48,12 @@ const buildCSS = async (args) => {
   try {
     const startTime = new Date().getTime();
 
-    const options = getOptions(args);
-    console.log(options.folder);
-    const srcFolder = options.folder ? `./${options.folder}` : './src';
-    const cssFilesToRead = options.build === '--ie' ? [] : importFrom;
+    const { build } = getOptions(args);
+
+    const srcFolder = build.indexOf('utilities') ? `./src/utilities` : './src';
+    const cssFilesToRead = importFrom;
     const postcssPlugins = [postcssImport];
-    let outFolder = options.build ? 'docs_src/css' : 'dist';
+    let outFolder = build ? 'docs_src/css' : 'dist';
 
     const fileTypeToFind = '.css';
 
@@ -91,8 +63,12 @@ const buildCSS = async (args) => {
     readFolder(srcFolder, fileTypeToFind, cssFilesToRead);
     cssFilesToRead.push('./node_modules/@ekstra-bladet/eb-colors/dist/eb-colors-classes.css');
     console.log('cssFilesToRead', cssFilesToRead);
-    const outputFileName = `eb-designsystem${options.build}.css`;
-    const outputFile = `${outFolder}/${outputFileName}`;
+    const outputFileName = build ? `eb-designsystem--${build}.css` : `eb-designsystem.css`;
+    const outputFiles = [`${outFolder}/${outputFileName}`];
+
+    if (build === 'utilities') {
+      outputFiles.push(`dist/${outputFileName}`);
+    }
 
     if (!fs.existsSync(outFolder)) {
       fs.mkdirSync(outFolder);
@@ -113,11 +89,13 @@ const buildCSS = async (args) => {
       .then((result) => {
         try {
           const resultCss = result.css;
-          fs.writeFile(outputFile, resultCss, () => {
-            return true;
-          });
 
-          console.log(`postcss PRODUCTION ready -> ${outputFile}`);
+          outputFiles.forEach((outputFile) => {
+            fs.writeFile(outputFile, resultCss, () => {
+              return true;
+            });
+            console.log(`postcss PRODUCTION ready -> ${outputFile}`);
+          });
         } catch (err) {
           throw err;
         }
