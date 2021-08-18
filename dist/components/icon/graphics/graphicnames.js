f1829324b80fb0dbd72f805438c68a1d2a03b7c8
tegner <jesper.h.tegner@jppol.dk>
2021-08-18 13:12:57 +0200

    remove dist from github

--- dist/components/icon/graphics/graphicnames.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.graphicnames = void 0;
-exports.graphicnames = ['ebupdate', 'ekstrabladet'];
--- dist/components/icon/svgs/iconnames.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.iconnames = void 0;
-exports.iconnames = ['angledown', 'angleleft', 'angleright', 'angleup', 'article', 'at', 'check', 'clock', 'creditcard', 'ebplus_icon', 'ebplus_sort', 'envelope', 'figcaptionpin', 'gallery', 'headphones', 'headset', 'historyregular', 'lightning', 'lock', 'medielogin', 'menubars', 'mitebregular', 'mitebsolid', 'newspaper', 'phone', 'play', 'rss', 'smartphone', 'starregular', 'tagregular', 'tagsolid', 'tagsregular', 'tagssolid', 'toggleoff', 'toggleon', 'video'];
