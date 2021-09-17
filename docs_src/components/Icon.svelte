08f8b09b244f167397c815a5da0ac110d42dcd19
MathiasNordby <mathias_nordby@hotmail.com>
2021-09-17 13:04:11 +0200

    New Icons from fa-light

--- docs_src/components/Icon.svelte
+++ docs_src/components/Icon.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
   import Prism from 'svelte-prism';
   import { sourceType } from '../stores';
-  import { Badge, Card, Icon } from '../../dist';
+  import { Badge, Card, Icon, Button } from '../../dist';
   import { iconnames } from '../../dist/components/icon/svgs/iconnames';
   import { graphicnames } from '../../dist/components/icon/graphics/graphicnames';
+
+  let size = 34;
+
+  const decrement = () => {
+    return --size;
+  };
+
+  const increment = () => {
+    return ++size;
+  };
 </script>
 
 <h1 class="color--eb">Icon library</h1>
@@ -27,7 +37,7 @@
         <td>className</td>
         <td>string</td>
         <td />
-        <td>Used to select icon if type is set to 'fa'</td>
+        <td>Custom class names</td>
       </tr>
       <tr>
         <td>name <Badge type="primary" extension="small">required</Badge></td>
@@ -37,21 +47,21 @@
       </tr>
       <tr>
         <td>type</td>
-        <td>'svg' | 'fa'</td>
+        <td>'svg'</td>
         <td>svg</td>
-        <td>Use EB svg icons or FontAwesome icons</td>
+        <td>Use EB svg icons</td>
       </tr>
       <tr>
         <td>width</td>
         <td>number</td>
         <td>36</td>
-        <td>The widthof the icon in pixels. Only on 'svg' mode</td>
+        <td>The width of the icon in pixels</td>
       </tr>
       <tr>
         <td>style</td>
         <td>string</td>
         <td />
-        <td>Custom styling. Only on 'svg' mode</td>
+        <td>Custom styling</td>
       </tr>
     </tbody>
   </table>
@@ -62,10 +72,24 @@
   Icons are simple and dynamic only made with fill. Fill has the value currentColor and makes it possible to add the
   desired color to the icon.
 </p>
+<p>
+  Most icons are derived from <a href="https://fontawesome.com/" target="_blank">FontAwesome</a> using only the
+  <b>light</b>
+  or <b>solid</b> library!
+</p>
+<div class="flex flex-row flex-justify--center flex-align--center">
+  <Button className="margin-m--r" type="cancel" on:click={decrement}>
+    <Icon name="chevronleft" style="width: 18px; height: 18px;" />
+  </Button>
+  <p>Size: {size}px</p>
+  <Button className="margin-m--l" type="cancel" on:click={increment}>
+    <Icon name="chevronright" style="width: 18px; height: 18px;" />
+  </Button>
+</div>
 <div class="flex flex-wrap--wrap">
   {#each iconnames as name}
     <Card className="flex-align--center flex-justify--center margin-s padding-m">
-      <Icon {name} className="margin-s" style="width: 36px; height: 36px;" />
+      <Icon {name} className="margin-s" style="width: {size}px; height: {size}px;" />
       <small>{name}</small>
     </Card>
   {/each}
@@ -104,18 +128,5 @@
     {`<svg viewBox="0 0 50 50">
   <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon_name"></use>
 </svg>`}
-  </Prism>
-{/if}
-
-<h3><a href="https://fontawesome.com/" target="_blank">Font Awesome</a> icons</h3>
-<Icon type="fa" className="fas fa-snowplow" />
-
-{#if $sourceType === 'svelte'}
-  <Prism language="html">
-    {`<Icon type="fa" className="fas fa-snowplow" />`}
-  </Prism>
-{:else}
-  <Prism language="html">
-    {`<i class="fas fa-snowplow"></i>`}
   </Prism>
 {/if}
