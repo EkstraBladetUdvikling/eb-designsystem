3645a296bc37ee7c32662532251e9ffded179742
tegner <jesper.h.tegner@jppol.dk>
2022-01-26 17:00:13 +0100

    EBCD-14654 . rebuild

--- src/components/articlecard/ArticleCard.svelte
+++ src/components/articlecard/ArticleCard.svelte
@@ -84,7 +84,7 @@
     <div class={innerClass}>
       {#if premium}
         <div class="premium-dogear">
-          <Icon className="color--white" name="ebplus" width="20" />
+          <Icon className="color--white" name="ebplusred" width="20" />
         </div>
       {/if}
       {#if loading}
