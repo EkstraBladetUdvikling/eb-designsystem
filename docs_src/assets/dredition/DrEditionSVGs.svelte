5277133b5f967e50d5089e7deaf5d4e01a82e0a9
tegner <jesper.h.tegner@jppol.dk>
2022-02-02 12:51:54 +0100

    EBCD-14704 . remove hover effect from cards (#183)
    * EBCD-14704
    
    * EBCD-14704 . swipelist
    
    * EBCD-14704 . swipelist
    
    * build js

--- docs_src/assets/dredition/DrEditionSVGs.svelte
+++ docs_src/assets/dredition/DrEditionSVGs.svelte
@@ -15,91 +15,91 @@
         <td>Hvidt plus med sort skygge (Kan bruges på alle baggrunde, der ikke er røde)</td>
         <td>eb-plus-white-onblack-anim.svg</td>
         <td style="background-color: black">
-          <img src="./svg/dredition/eb-plus-white-onblack-anim.svg" height="50" />
+          <img alt="" src="./svg/dredition/eb-plus-white-onblack-anim.svg" height="50" />
         </td>
       </tr>
       <tr>
         <td>Hvidt plus med rød skygge (Kan bruges på rød baggrund)</td>
         <td>eb-plus-white-onred-anim.svg</td>
         <td style="background-color: darkred">
-          <img src="./svg/dredition/eb-plus-white-onred-anim.svg" height="50" />
+          <img alt="" src="./svg/dredition/eb-plus-white-onred-anim.svg" height="50" />
         </td>
       </tr>
       <tr>
         <td>Rød plus med sort skygge (Kan bruges på hvid baggrund)</td>
         <td>eb-plus-red-anim.svg</td>
         <td style="background-color: white">
-          <img src="./svg/dredition/eb-plus-red-anim.svg" height="50" />
+          <img alt="" src="./svg/dredition/eb-plus-red-anim.svg" height="50" />
         </td>
       </tr>
       <tr>
         <td>Lige nu</td>
         <td>ligenu.svg</td>
         <td>
-          <img src="./svg/dredition/ligenu.svg" />
+          <img alt="" src="./svg/dredition/ligenu.svg" />
         </td>
       </tr>
       <tr>
         <td>Breaking</td>
         <td>breaking.svg</td>
         <td>
-          <img src="./svg/dredition/breaking.svg" />
+          <img alt="" src="./svg/dredition/breaking.svg" />
         </td>
       </tr>
       <tr>
         <td>Breaking swipe</td>
         <td>breaking-swipe.svg</td>
         <td>
-          <img src="./svg/dredition/breaking-swipe.svg" />
+          <img alt="" src="./svg/dredition/breaking-swipe.svg" />
         </td>
       </tr>
       <tr>
         <td>Af$løring swipe</td>
         <td>afsloering-swipe.svg</td>
         <td>
-          <img src="./svg/dredition/afsloering-swipe.svg" />
+          <img alt="" src="./svg/dredition/afsloering-swipe.svg" />
         </td>
       </tr>
       <tr>
         <td>AMA live</td>
         <td>ama-live.svg</td>
         <td>
-          <img src="./svg/dredition/ama-live.svg" />
+          <img alt="" src="./svg/dredition/ama-live.svg" />
         </td>
       </tr>
       <tr>
         <td>AMA live opaque</td>
         <td>ama-live-opaque.svg</td>
         <td>
-          <img src="./svg/dredition/ama-live-opaque.svg" />
+          <img alt="" src="./svg/dredition/ama-live-opaque.svg" />
         </td>
       </tr>
       <tr>
         <td>Liveblog</td>
         <td>liveblog.svg</td>
         <td>
-          <img src="./svg/dredition/liveblog.svg" />
+          <img alt="" src="./svg/dredition/liveblog.svg" />
         </td>
       </tr>
       <tr>
         <td>Liveblog opaque</td>
         <td>liveblog-opaque.svg</td>
         <td>
-          <img src="./svg/dredition/liveblog-opaque.svg" />
+          <img alt="" src="./svg/dredition/liveblog-opaque.svg" />
         </td>
       </tr>
       <tr>
         <td>live video</td>
         <td>live-video.svg</td>
         <td>
-          <img src="./svg/dredition/live-video.svg" />
+          <img alt="" src="./svg/dredition/live-video.svg" />
         </td>
       </tr>
       <tr>
         <td>live video opaque</td>
         <td>live-video-opaque.svg</td>
         <td>
-          <img src="./svg/dredition/live-video-opaque.svg" />
+          <img alt="" src="./svg/dredition/live-video-opaque.svg" />
         </td>
       </tr>
     </table>
@@ -116,49 +116,49 @@
         <td>ama</td>
         <td>ama-static.svg</td>
         <td>
-          <img src="./svg/dredition/ama-static.svg" />
+          <img alt="" src="./svg/dredition/ama-static.svg" />
         </td>
       </tr>
       <tr>
         <td>Audio</td>
         <td>audio.svg</td>
         <td>
-          <img src="./svg/dredition/audio.svg" />
+          <img alt="" src="./svg/dredition/audio.svg" />
         </td>
       </tr>
       <tr>
         <td>Plus</td>
         <td>eb-plus.svg</td>
         <td>
-          <img src="./svg/dredition/eb-plus.svg" />
+          <img alt="" src="./svg/dredition/eb-plus.svg" />
         </td>
       </tr>
       <tr>
         <td>updating</td>
         <td>updating.svg</td>
         <td>
-          <img src="./svg/dredition/updating.svg" />
+          <img alt="" src="./svg/dredition/updating.svg" />
         </td>
       </tr>
       <tr>
         <td>Updating static</td>
         <td>updating-static.svg</td>
         <td>
-          <img src="./svg/dredition/updating-static.svg" />
+          <img alt="" src="./svg/dredition/updating-static.svg" />
         </td>
       </tr>
       <tr>
         <td>Video</td>
         <td>video.svg</td>
         <td>
-          <img src="./svg/dredition/video.svg" />
+          <img alt="" src="./svg/dredition/video.svg" />
         </td>
       </tr>
       <tr>
         <td>Soccerball</td>
         <td>soccerball.svg</td>
         <td>
-          <img src="./svg/dredition/soccerball.svg" />
+          <img alt="" src="./svg/dredition/soccerball.svg" />
         </td>
       </tr>
     </table>
@@ -174,9 +174,5 @@
 
   .dredition-table-icons td {
     border: 1px solid black;
-  }
-
-  .dredition-table-icons td svg {
-    height: 75px;
   }
 </style>
