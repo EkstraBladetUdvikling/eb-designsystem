e0af3c67a15016163f554bc600910b8b391dd56f
tegner <jesper.h.tegner@jppol.dk>
2022-03-15 11:49:47 +0100

    Updated dependencies and corrected imports

--- src/functions/tooltipStore.ts
+++ src/functions/tooltipStore.ts
@@ -1,5 +1,7 @@
+import type { Writable } from 'svelte/store';
 import type { Instance, Props, RenderProps } from 'tippy.js/headless';
-import { writable, Writable } from 'svelte/store';
+
+import { writable } from 'svelte/store';
 
 export type TTooltipInstance = Instance<Partial<Omit<Props, keyof RenderProps>>>;
 
