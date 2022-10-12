c54c9c02d29c6e400c3255d91b1bea8d4280bbfe
Mikkel B. Jensen <mikkel.jensen@eb.dk>
2022-10-12 10:38:30 +0200

    Bundle

--- src/functions/tooltipAction.ts
+++ src/functions/tooltipAction.ts
@@ -2,7 +2,7 @@ import type { ITooltipOptions } from '../types/tooltipAction';
 
 import Tooltip from '../components/tooltip/Tooltip.svelte';
 
-export default function (anchorNode: HTMLElement, options: ITooltipOptions) {
+export default function(anchorNode: HTMLElement, options: ITooltipOptions) {
   const target = document.createElement('div');
   const instance = new Tooltip({
     props: { anchorNode, ...options },
