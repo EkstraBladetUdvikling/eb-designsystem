0754cedf6560c885ef84ffc6c1b2fa11ee128f98
Mikkel B. Jensen <mbjedk@users.noreply.github.com>
2022-10-12 10:36:23 +0200

    Update tooltipAction.ts (#216)

--- src/functions/tooltipAction.ts
+++ src/functions/tooltipAction.ts
@@ -2,7 +2,7 @@ import type { ITooltipOptions } from '../types/tooltipAction';
 
 import Tooltip from '../components/tooltip/Tooltip.svelte';
 
-export default function(anchorNode: HTMLElement, options: ITooltipOptions) {
+export default function (anchorNode: HTMLElement, options: ITooltipOptions) {
   const target = document.createElement('div');
   const instance = new Tooltip({
     props: { anchorNode, ...options },
@@ -14,7 +14,7 @@ export default function(anchorNode: HTMLElement, options: ITooltipOptions) {
       instance.$destroy();
     },
     update(newProps: ITooltipOptions) {
-      Object.assign(instance, newProps);
+      instance.$set(newProps);
     },
   };
 }
