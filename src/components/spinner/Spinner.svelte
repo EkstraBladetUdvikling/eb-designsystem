91d68ec4af66c3024a5fa8c10106ebf3f686c819
tegner <jesper.h.tegner@jppol.dk>
2022-12-16 12:39:44 +0100

    delayed spinner (#225)
    * delayed spinner
    
    * add size option

--- src/components/spinner/Spinner.svelte
+++ src/components/spinner/Spinner.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
   import Icon from '../icon/Icon.svelte';
-  export let isLoading = false;
+
+  export let delay = 150;
+  export let size = 18;
+
+  let isLoading = false;
+
+  setTimeout(() => {
+    isLoading = true;
+  }, delay);
+
+  $: style = `--icon-size: ${size}px;`;
 </script>
 
 {#if isLoading}
-  <div class="loader flex flex--center">
-    <Icon className="bounce bounce1" name="circle-solid" style="width: 18px; height: 18px;" />
-    <Icon className="bounce bounce2" name="circle-solid" style="width: 18px; height: 18px;" />
-    <Icon className="bounce bounce3" name="circle-solid" style="width: 18px; height: 18px;" />
+  <div class="loader flex flex--center" {style}>
+    <Icon className="bounce bounce1" name="circle-solid" />
+    <Icon className="bounce bounce2" name="circle-solid" />
+    <Icon className="bounce bounce3" name="circle-solid" />
   </div>
 {/if}
