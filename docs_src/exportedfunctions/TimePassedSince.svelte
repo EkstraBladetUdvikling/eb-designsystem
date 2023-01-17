c29fcb7d5ce253e14f7bf02f2211d1fd347c8553
Mathias Nordby <mathias.nordby@eb.dk>
2023-01-17 13:57:25 +0100

    todayAsText (#227)

--- docs_src/exportedfunctions/TimePassedSince.svelte
+++ docs_src/exportedfunctions/TimePassedSince.svelte
@@ -7,6 +7,7 @@
 <code>
   <div class="padding-xl--l padding-m--tb">
     <p><b>@param datetime</b> valid datetimestring <em>required</em></p>
+    <p><b>@param todayAsText</b> boolean <em>optional</em></p>
   </div>
 </code>
 
