2123bbcd09140a383eef7158c7c5bbcc3de275a5
tegner <jesper.h.tegner@jppol.dk>
2023-08-15 14:49:00 +0200

    EBCD-16206 . dependency upgrade

--- src/components/icon/graphics/graphicnames.ts
+++ src/components/icon/graphics/graphicnames.ts
@@ -2,3 +2,4 @@
   import type { GraphicTypes } from '../../../types/Icon';
   export const graphicnames: string[] = ['ebplusblack','ebpluswhiteblackshadow','ebpluswhite','ekstrabladet','figcaptionpin','videographic'];
   export const graphicnameshtml: GraphicTypes[] = ['ebplus-black','ebplus-white-blackshadow','ebplus-white','ekstrabladet','figcaption-pin','video-graphic'];
+  
