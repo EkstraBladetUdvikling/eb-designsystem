6354d739e4e95331545463d9bd1920544fe82f1c
tegner <jesper.h.tegner@jppol.dk>
2023-10-20 12:04:15 +0200

    new volume graphic

--- src/components/icon/graphics/graphicnames.ts
+++ src/components/icon/graphics/graphicnames.ts
@@ -1,5 +1,5 @@
 
   import type { GraphicTypes } from '../../../types/Icon';
-  export const graphicnames: string[] = ['ebplusblack','ebpluswhiteblackshadow','ebpluswhite','ekstrabladet','figcaptionpin','videographic'];
-  export const graphicnameshtml: GraphicTypes[] = ['ebplus-black','ebplus-white-blackshadow','ebplus-white','ekstrabladet','figcaption-pin','video-graphic'];
+  export const graphicnames: string[] = ['ebplusblack','ebpluswhiteblackshadow','ebpluswhite','ekstrabladet','figcaptionpin','videographic','volumemutecolor'];
+  export const graphicnameshtml: GraphicTypes[] = ['ebplus-black','ebplus-white-blackshadow','ebplus-white','ekstrabladet','figcaption-pin','video-graphic','volume-mute-color'];
   
