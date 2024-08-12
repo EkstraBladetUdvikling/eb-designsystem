41c5ad33505b7f7614e944658b418eab20576bed
tegner <jesper.h.tegner@jppol.dk>
2024-08-12 15:07:38 +0200

    remove logs

--- node/copy_svg.mjs
+++ node/copy_svg.mjs
@@ -3,12 +3,10 @@ import { copyFileSync, constants, existsSync, mkdirSync, rmSync } from "fs";
 const distFolder = "./dist";
 const DESTINATION = `${distFolder}/icons.svg`;
 if (!existsSync(distFolder)) {
-  console.log("The path does not exist.");
   mkdirSync(distFolder);
 }
 
 if (existsSync(DESTINATION)) {
-  console.log("The path exists.");
   rmSync(DESTINATION);
 }
 
