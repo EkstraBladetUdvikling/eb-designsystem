1243c4fd5a97a3b6214e41cf7a9e39f1699eba26
tegner <jesper.h.tegner@jppol.dk>
2024-08-12 10:33:31 +0200

    removed linting

--- node/linter.mjs
+++ node/linter.mjs
@@ -1,41 +1,43 @@
-import { ESLint } from "eslint";
+// import { ESLint } from "eslint";
 
-async function linter() {
-  await (async function main() {
-    const srcFilesToLint = `./src/**/*.{svelte,ts}`;
-    const docfilesToLint = `./docs_src/**/*.{svelte,ts}`;
-    const eslint = new ESLint({
-      cache: true,
-      cacheStrategy: "content",
-      fix: true,
-    });
+// async function linter() {
+//   await (async function main() {
+//     const srcFilesToLint = `./src/**/*.{svelte,ts}`;
+//     const docfilesToLint = `./docs_src/**/*.{svelte,ts}`;
+//     const eslint = new ESLint({
+//       cache: true,
+//       cacheStrategy: "content",
+//       fix: true,
+//     });
 
-    // 2. Lint files. This doesn't modify target files.
-    const results = await eslint.lintFiles([srcFilesToLint, docfilesToLint]);
+//     // 2. Lint files. This doesn't modify target files.
+//     const results = await eslint.lintFiles([srcFilesToLint, docfilesToLint]);
 
-    // 3. Modify the files with the fixed code.
-    await ESLint.outputFixes(results);
+//     // 3. Modify the files with the fixed code.
+//     await ESLint.outputFixes(results);
 
-    // 4. Format the results.
-    const formatter = await eslint.loadFormatter("stylish");
-    const resultText = formatter.format(results);
+//     // 4. Format the results.
+//     const formatter = await eslint.loadFormatter("stylish");
+//     const resultText = formatter.format(results);
 
-    const hasErrors = results.find((res) => res.errorCount > 0);
+//     const hasErrors = results.find((res) => res.errorCount > 0);
 
-    if (hasErrors) {
-      // 5. Output it.
-      // eslint-disable-next-line no-console
-      console.log(resultText);
+//     if (hasErrors) {
+//       // 5. Output it.
+//       // eslint-disable-next-line no-console
+//       console.log(resultText);
 
-      process.exit(1);
-    } else {
-      // eslint-disable-next-line no-console
-      console.log("Passed");
-    }
-  })().catch((error) => {
-    process.exitCode = 1;
-    console.error(error);
-  });
-}
+//       process.exit(1);
+//     } else {
+//       // eslint-disable-next-line no-console
+//       console.log("Passed");
+//     }
+//   })().catch((error) => {
+//     process.exitCode = 1;
+//     console.error(error);
+//   });
+// }
 
-linter();
+// linter();
+
+console.log("should be linting");
