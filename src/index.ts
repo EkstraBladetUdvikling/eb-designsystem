470966a1348b121e1f4ead280b2aed8e299062df
tegner <jtegner@gmail.com>
2024-11-04 14:13:19 +0100

    remove Tabs

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,6 @@ export { default as ArticleCard } from './components/articlecard/ArticleCard.sve
 export { default as HorizontalScroll } from './components/horizontalScroll/HorizontalScroll.svelte';
 export { default as Icon } from './components/icon/Icon.svelte';
 export { default as Spinner } from './components/spinner/Spinner.svelte';
-export { default as Tab } from './components/tabs/Tab.svelte';
-export { default as TabContent } from './components/tabs/TabContent.svelte';
-export { default as TabList } from './components/tabs/TabList.svelte';
-export { default as Tabs } from './components/tabs/Tabs.svelte';
 
 // Functions
 export { timePassedSince } from './functions/timepassedsince';
