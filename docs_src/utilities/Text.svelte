76e8d3d4a400c3a96fe9251c0944530ed212a13f
Nathalie <ditzynat@gmail.com>
2025-04-25 14:05:15 +0200

    RE-2
    Added new fonts for titles and bodytext, with associated lineheights - version bump to 8.4.0

--- docs_src/utilities/Text.svelte
+++ docs_src/utilities/Text.svelte
@@ -35,14 +35,16 @@
 <div class="card bg--graa7">
   <div class="card-content flex flex-justify--between">
     <div class="width-1of3 margin-m">
-      <p>normal line-height</p>
-      <p>normal line-height</p>
-      <p>normal line-height</p>
+      <p class="lineheight-m">normal line-height</p>
+      <p class="lineheight-l">sub-overskrift/manchet line-height</p>
+      <p class="lineheight-xl">lead-in line-height</p>
+      <p class="lineheight-xxl">Overskrift(H1) line-height</p>
     </div>
-    <div class="width-1of3 margin-m lineheight-m">
-      <p>lineheight-m</p>
-      <p>lineheight-m</p>
-      <p>lineheight-m</p>
+    <div class="width-1of3 margin-m">
+      <p class="lineheight-m">lineheight-m</p>
+      <p class="lineheight-l">lineheight-l</p>
+      <p class="lineheight-xl">lineheight-xl</p>
+      <p class="lineheight-xxl">lineheight-xxl</p>
     </div>
   </div>
 </div>
