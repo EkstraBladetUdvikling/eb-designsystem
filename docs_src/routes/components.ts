f3a0b469793d14b9b0054ae1a46ca8243a967785
Lasse Godtkjaer <90180602+lassegodtkjaer@users.noreply.github.com>
2026-02-19 14:44:28 +0100

    Remove spinner component from old designsystem and docs

--- docs_src/routes/components.ts
+++ docs_src/routes/components.ts
@@ -4,7 +4,6 @@ import ButtonGroup from '../components/ButtonGroup.svelte';
 import Card from '../components/Card.svelte';
 import HorizontalScroll from '../components/HorizontalScroll.svelte';
 import Icon from '../components/Icon.svelte';
-import Spinner from '../components/Spinner.svelte';
 
 export const components = {
   href: '/components',
@@ -38,11 +37,6 @@ export const components = {
       component: HorizontalScroll,
       href: '/components/horizontalscroll',
       title: 'Horizontal scroll',
-    },
-    {
-      component: Spinner,
-      href: '/components/spinner',
-      title: 'Spinner',
     },
   ],
   title: 'Components',
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 export { default as ArticleCard } from './components/articlecard/ArticleCard.svelte';
 export { default as HorizontalScroll } from './components/horizontalScroll/HorizontalScroll.svelte';
 export { default as Icon } from './components/icon/Icon.svelte';
-export { default as Spinner } from './components/spinner/Spinner.svelte';
 
 // Functions
 export { timePassedSince } from './functions/timepassedsince';
